4e01a764fa769543437b70ae9ce08a6bc9af8809
Ananda Bayu <anandabayuf@github.com>
2020-04-13 18:10:36 +0700

    commit dependencies

---
+++ src/Main/CabNavigator.java
@@ -0,0 +1,50 @@
+/*
+ * Created By Ananda Bayu Fauzan.
+ */
+package Main;
+
+/**
+ *
+ * @author Ananda Bayu
+ */
+public class CabNavigator {
+    private Cab cab;
+    private double speed;
+    private int floorNumber;
+    
+    public CabNavigator(){
+        
+    }
+    
+    void MoveToFloor(int floorNumber){
+        
+    }
+    
+    void CalculateNewDirection(){
+        
+    }
+    
+    void CalculateNewSpeed(){
+        
+    }
+    
+    void CalculateNewPosition(){
+        
+    }
+    
+    boolean IsCabAtDestinationFloor(){
+        return false;
+    }
+    
+    void MarkerDetected(){//cabID
+        
+    }
+    
+    void Suspend(){
+        
+    }
+    
+    void Resume(){
+        
+    }
+}
---
+++ src/Main/FloorRequestButton.java
@@ -0,0 +1,22 @@
+/*
+ * Created By Ananda Bayu Fauzan.
+ */
+package Main;
+
+/**
+ *
+ * @author Ananda Bayu
+ */
+public class FloorRequestButton {
+    void pressed(){
+        
+    }
+    
+    void turnLightOn(){
+        
+    }
+    
+    void turnLightOff(){
+        
+    }
+}
