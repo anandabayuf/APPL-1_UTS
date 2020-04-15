161c48219d8d62007fa5529321f840f9faa969da
Evan Lokajaya <48423337+lokajayae@users.noreply.github.com>
2020-04-15 22:32:14 +0700

    OpenDoor, EmergencyButton, EmergencyBell

--- src/Model/Cab.java
+++ src/Model/Cab.java
@@ -5,6 +5,9 @@ package Model;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,6 +16,7 @@ import java.util.Map;
 public class Cab {
     private Floor floor;
     private Direction direction;
+    private Door door;
     
     public enum Direction{
         Up, Down;
@@ -39,44 +43,57 @@ public class Cab {
         public static Floor valueOf(int floor) {
             return (Floor) map.get(floor);
         }
-
+        
+        public void next(){
+            value++;
+        }
+        
+        public void before(){
+            value++;
+        }
         public int getValue() {
             return value;
         }
     }
     
+    public enum Door{
+        Closed,
+        Open; 
+    }
+    
     public Cab(Direction direction, Floor floor){
         this.direction = direction;
         this.floor = floor;
+        this.door = Door.Closed;
     }
 
-    /**
-     * @return the floor
-     */
     public Floor getFloor() {
         return floor;
     }
 
-    /**
-     * @param floor the floor to set
-     */
     public void setFloor(Floor floor) {
         this.floor = floor;
     }
 
-    /**
-     * @return the direction
-     */
     public Direction getDirection() {
         return direction;
     }
 
-    /**
-     * @param direction the direction to set
-     */
     public void setDirection(Direction direction) {
         this.direction = direction;
     }
     
+    public void setDoorStatus(Door status){
+        this.door = status;
+    }
     
+    public Door getDoorStatus(){
+        return this.door;
+    }
+    
+    public void startTimerDoor(){
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException ex) {}
+    }
 }
